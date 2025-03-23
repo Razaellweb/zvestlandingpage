cc8c341bb4317cd7f0371ed600643474a1c3a511
berryboylb <phemmynesce4life@gmail.com>
2025-03-23 21:40:04 +0100

    first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Olorunfemi Daramola",
+  description: "this is my site",
+  keywords: ["software"],
+  url: "https://olorun.femi",
+  ogImage: "https://olorun.femi",
+  links: {
+    twitter: "https://x.com/pappychulow",
+    github: "https://github.com/berryboylb",
+  },
+};
