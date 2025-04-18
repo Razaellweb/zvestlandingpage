307ecc2821b6007163c2b14220afc633c770d7c9
berryboylb <phemmynesce4life@gmail.com>
2025-04-18 20:32:10 +0100

    first commit

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,1 +1,6 @@
-export {};
+import Logo from "./logo.svg";
+import Gift from "./gift.svg";
+import TopCurve from "./top-curve.svg";
+import BottomCurve from "./bottom-curve.svg";
+
+export { Logo, Gift, TopCurve, BottomCurve };
--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,11 @@
 import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Olorunfemi Daramola",
-  description: "this is my site",
+  name: "Ticketeur",
+  description: "From booking tickets to managing events, Ticketeur is your comprehensive, easy-to-use platform for all event experiences.",
   keywords: ["software"],
-  url: "https://olorun.femi",
-  ogImage: "https://olorun.femi",
+  url: "https://useticketeur.com",
+  ogImage: "https://useticketeur.com",
   links: {
     twitter: "https://x.com/pappychulow",
     github: "https://github.com/berryboylb",
